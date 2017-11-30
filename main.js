f698bea3773cb57e4ae98c3a2d5b98c6e1b2a38b
Colin PUY <colin.puy@bonitasoft.com>
2017-11-30 09:51:35 +0100

    remove node-integration cause of moment

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.resolve(__dirname, 'frontend', 'build', 'dev', 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
