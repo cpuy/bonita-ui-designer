0bdbfdf63c68a02424413e91e48eeeea9fc0ef37
Colin PUY <colin.puy@bonitasoft.com>
2017-12-05 08:50:35 +0100

    improve page-repo decorator

--- frontend/backend/page-repository.decorator.js
+++ frontend/backend/page-repository.decorator.js
@@ -1,15 +1,24 @@
 const pages = [];
+let lastSavedState = JSON.stringify({});
 
 module.exports = {
 
   create: (artifact) => {
     artifact.id = artifact.name;
+    artifact.data = {};
     pages.push(artifact);
     return Promise.resolve(artifact);
   },
 
   all: () => Promise.resolve(pages),
 
-  load: (id) => Promise.resolve({ data: pages.find(p => p.id === id) || {}})
+  load: (id) => Promise.resolve({ data: pages.find(p => p.id === id) || {}}),
+
+  needSave: () => false,
+
+  setLastSavedState: (artifact) => {
+    //stringify artifact is faster and then lighter than copying it
+    lastSavedState = JSON.stringify(artifact);
+  }
 
 };
