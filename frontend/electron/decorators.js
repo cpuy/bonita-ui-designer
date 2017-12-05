9a65b6abecfb4f14600ae774cebb42cf28865c8a
Colin PUY <colin.puy@bonitasoft.com>
2017-12-05 09:01:04 +0100

    rearrange

---
+++ frontend/electron/decorators.js
@@ -0,0 +1,8 @@
+// provide decorators for angular services fetching data 
+window.angular
+  .module('uidesigner')
+  .config([ '$provide', function($provide) {
+    $provide.decorator('repositories', () => require('./repositories/repositories.decorator'));
+    $provide.decorator('pageRepo', () => require('./repositories/page-repository.decorator'));
+    $provide.decorator('widgetRepo', () => require('./repositories/widget-repository.decorator'));
+  }]);
