f9d5de3db37d7217ef8d74c6c16e0af1d443332d
Colin PUY <colin.puy@bonitasoft.com>
2017-12-11 13:45:27 +0100

    add custom false and get widgets

--- frontend/electron/repositories/widget-repository.decorator.js
+++ frontend/electron/repositories/widget-repository.decorator.js
@@ -1,9 +1,15 @@
 const widgets = [];
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = {
 
   customs: () => Promise.resolve(widgets),
-  all: () => Promise.resolve(widgets)
+  all: async () => {
+    let widgetsDir = path.resolve(__dirname, 'widgets');
+    const widgets = await fs.readdir(widgetsDir);
+    return widgets.map(f => require(path.resolve(widgetsDir, f, `${f}.json`)))
+  }
 
 
 };
